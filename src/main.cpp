dadc2e9ae222f6932d3263dc3db3498e58303eef
Andreas Johansson <aramande@hackular.com>
2011-06-14 19:35:45 +0200

    Implementing the make buildscript

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <cxxtest/ErrorPrinter.h>
+
+int main(int argc, char **argv){
+	CxxTest::ErrorPrinter().run();
+	return 0;
+}
---
+++ src/spritetest.h
@@ -0,0 +1,12 @@
+#include <cxxtest/TestSuite.h>
+#include "sprite.h"
+
+class SpriteTest : public CxxTest::TestSuite 
+{
+ public:
+  void testFunction( void )
+  {
+	  TS_ASSERT_EQUALS( 1, 1 )
+  }
+};
+

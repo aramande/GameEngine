622d43b3a8a81c405666f16530f77271cc3d487b
Aramande <aramande@hackular.com>
2011-07-07 13:21:52 +0200

    Filling out button.h with a few comments and a new constructor.

--- src/button.h
+++ src/button.h
@@ -7,6 +7,8 @@
 
 namespace engine {
 	typedef int Event; // temporary
+	typedef int Image;
+
 	typedef void (*FPoint)(Event); 
 	class Button : public Component {
 		std::string text;
@@ -14,7 +16,23 @@ namespace engine {
 	protected:
 	public:
 		Button(int x, int y, int w, int h);
+		Button(int x, int y, Image buttonImg);
+
+		/**
+		 * Set what should happen when the button is pressed down.
+		 * Will callback `code` whenever the button receives a 
+		 * pressed mouse event within it's coordinates
+		 *
+		 * @param code A function that takes a single Event argument.
+		 */
 		void buttonDown(FPoint code);
+
+		/**
+		 * Set what should happen when the button is pressed down.
+		 * Will callback `code` whenever the button receives a released mouse event.
+		 *
+		 * @param code A function that takes a single Event argument.
+		 */
 		void buttonUp(FPoint code);
 		
 	};

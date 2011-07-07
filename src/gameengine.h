667ff9a2a327be52e7ce562b0fb2af8c1391b270
Aramande <aramande@hackular.com>
2011-07-07 14:05:40 +0200

    Adding the Gametest rule to cmake and modifying the readme accordingly.

--- src/gameengine.h
+++ src/gameengine.h
@@ -4,25 +4,28 @@
 #include <map>
 #include <vector>
 #include "sprite.h"
+#include "window.h"
 #include "gui.h"
 
 namespace engine {
 	class GameEngine {
-		std::map<int, std::vector<Sprite*> > *fridge;
-		Gui *hud; 
+		std::map<int, std::vector<Sprite*> > *storage;
+		Gui* hud; 
+		Window* screen;
+		static GameEngine *instance;
 		
 		/** 
 		 * Runs through sprites, checking every vector that contains more than one value if they collide.
 		 * Incase of collision, onCollision is called on both sprites.
 		 */
 		void collide();
-		GameEngine(); // init the game engine here
+		GameEngine(Window* screen); // init the game engine here
 	public:
-		GameEngine *init();
+		static GameEngine* init(Window* screen);
 		/**
 		 * Add a sprite into the fridge at the right hash value. If another sprite has already created a vector, put it at the end of it.
 		 */
-		void addSprite(Sprite *s);
+		void addSprite(Sprite* s);
 		
 		/**
 		 * Runs the main loop and calls all necessary functions to draw sprites on the screen.

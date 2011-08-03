854de80dc02e002686b9ea2b3a38e0767638179c
Arian Jafari <aria-jaf@dsv.su.se>
2011-08-03 12:14:15 +0200

    Added second constructor to projectile, taking the origin sprite instead of hard coordinates.

--- src/projectile.cpp
+++ src/projectile.cpp
@@ -8,6 +8,13 @@ Projectile::Projectile(Image* image, int x, int y, int xvel, int yvel)
 	xVelocity = xvel;
 	yVelocity = yvel;
 }
+Projectile::Projectile(Image* image, Sprite* origin, bool top, int xvel, int yvel)
+	: Sprite(image, (origin->getWidth()/2) + origin->getX(), 
+	top ? origin->getY() : origin->getY() + origin->getHeight()) {
+
+	xVelocity = xvel;
+	yVelocity = yvel;
+}
 
 void Projectile::tick() {
 	translate(xVelocity, yVelocity);

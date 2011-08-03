a66a8515774aea916ea9611f7ce1950bd5ce0ecb
Aramande <aramande@hackular.com>
2011-08-03 12:25:22 +0200

    Removed autogenerated cconfig.h from repo

--- src/cconfig.h
+++
@@ -1,7 +0,0 @@
-// This file was generated using cmake, any modifications will be overwritten
-#define VERSION_MAJOR 1
-#define VERSION_MINOR 0
-#define VERSION_PATCH 0
-
-#define DATA_DIR "C:/Users/Arian/Documents/My Dropbox/DSV/Termin 3/PROG3/GameEngine/build/data"
-

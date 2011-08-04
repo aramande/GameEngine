2855158f28b72ea0d9765beb53f5f96783fcae75
Arian Jafari <aria-jaf@dsv.su.se>
2011-08-04 16:15:11 +0200

    Removed redundant constructor from projectile.

--- src/projectile.cpp
+++ src/projectile.cpp
@@ -2,12 +2,6 @@
 
 using namespace engine;
 
-Projectile::Projectile(Image* image, int x, int y, int xvel, int yvel) 
-	: Sprite(image, x, y) {
-	
-	xVelocity = xvel;
-	yVelocity = yvel;
-}
 Projectile::Projectile(Image* image, Sprite* origin, bool top, int xvel, int yvel)
 	: Sprite(image, (origin->getWidth()/2) + origin->getX(), 
 	top ? origin->getY() - (image->getHeight() + 1) 
--- src/projectile.h
+++ src/projectile.h
@@ -4,11 +4,7 @@
 #include "gameengine.h"
 
 class Projectile : public engine::Sprite {
-	int xVelocity;
-	int yVelocity;
-protected:
 public:
-	Projectile(engine::Image* image, int x, int y, int xvel, int yvel);
 	Projectile(engine::Image* image, engine::Sprite* origin, bool top, int xvel, int yvel);
 };
 

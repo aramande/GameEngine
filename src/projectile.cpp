a7b003fe2cab8518aa4ace81bf8d13a4502a2574
Arian Jafari <aria-jaf@dsv.su.se>
2011-08-04 15:48:27 +0200

    Moved 'velocity' from projectile to sprite. Default values are 0,0. Tick() automatically moves sprite according to its velocity.

--- src/projectile.cpp
+++ src/projectile.cpp
@@ -11,31 +11,5 @@ Projectile::Projectile(Image* image, int x, int y, int xvel, int yvel)
 Projectile::Projectile(Image* image, Sprite* origin, bool top, int xvel, int yvel)
 	: Sprite(image, (origin->getWidth()/2) + origin->getX(), 
 	top ? origin->getY() - (image->getHeight() + 1) 
-	: origin->getY() + origin->getHeight() + image->getHeight()) {
-
-	xVelocity = xvel;
-	yVelocity = yvel;
-}
-
-void Projectile::tick() {
-	translate(xVelocity, yVelocity);
-}
-
-int Projectile::getXVelocity() {
-	return xVelocity;
-}
-int Projectile::getYVelocity() {
-	return yVelocity;
-}
-
-void Projectile::setXVelocity(int xvel) {
-	xVelocity = xvel;
-}
-void Projectile::setYVelocity(int yvel) {
-	yVelocity = yvel;
-}
-
-void Projectile::setVelocity(int xvel, int yvel) {
-	xVelocity = xvel;
-	yVelocity = yvel;
+	: origin->getY() + origin->getHeight() + image->getHeight(), xvel, yvel) {
 }
--- src/projectile.h
+++ src/projectile.h
@@ -10,17 +10,6 @@ protected:
 public:
 	Projectile(engine::Image* image, int x, int y, int xvel, int yvel);
 	Projectile(engine::Image* image, engine::Sprite* origin, bool top, int xvel, int yvel);
-
-	void tick();
-
-	int getXVelocity();
-	int getYVelocity();
-
-	void setXVelocity(int xvel);
-	void setYVelocity(int yvel);
-
-	void setVelocity(int xvel, int yvel);
-
 };
 
 
